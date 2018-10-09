2fa84f1f095d033a0214b944a11802d22334ed31
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2018-10-09 09:21:38 -0500

    A simple bash script to automate the creation of: -A Git repo -Install the package JSON with default options -Create src directory with index.js -Open VIM

---
+++ init-js.sh
@@ -0,0 +1,6 @@
+echo "Initializing a simple JS project at $(pwd)"
+git init
+npm init -y #-y stands for all the defaults
+mkdir src
+touch src/index.js
+vim . #open index.js on vim, shift (:) and wq to save and quit
