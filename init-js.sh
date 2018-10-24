7e971f7ad6451eb1515b4e6e85cbfa980e40f936
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2018-10-24 11:03:48 -0500

    Added node-sass dependency, created the start command to output the SCSS code into CSS

--- init-js.sh
+++ init-js.sh
@@ -1,6 +1,10 @@
 echo "Initializing a simple JS project at $(pwd)"
 git init
 npm init -y #-y stands for all the defaults
-mkdir src
+npm install node-sass --save-dev
+mkdir src css scss
+echo ".red { color:red; } .red-dark { color: darken(red, 20%); }" >> scss/main.scss
 touch src/index.js
+sed -i 's/"scripts": {/"scripts": { \n\t\t"start": "node-sass -o css scss",/g' package.json
 vim . #open index.js on vim, shift (:) and wq to save and quit
+
